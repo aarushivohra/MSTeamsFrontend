032121b89ba4d5ff46eaf0e62ce3f8275b9bb82e
aarushivohra <aarushivohra01@gmail.com>
2021-06-16 17:18:54 +0530

    :sparkles: created folder structure

---
+++ src/views/public/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+export default function Home(props) {
+    console.log(props);
+    return (
+        <>
+        <div>
+            Hello
+        </div>
+        <div>
+            Wow
+        </div>
+        </>
+    )
+}
